7595bbf3046f4aeb54438ae3b6c6cc2439705e4a
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2020-11-08 19:52:35 -0300

    configurando o webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import createReactClass from 'create-react-class'
+
+const App = createReactClass({
+  render: function () {
+    return <h1>Batman</h1>
+  }
+})
+
+export default App
