199d7052b1b50162d936e8f2645033df2eece51b
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2020-12-08 20:52:32 -0300

    change no input

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 'use strict'
 
 import React, {Component} from 'react'
-import Button from './button'
 export default class App extends Component {
 
   render(){
     return (
       <div>
-        <Button handleClick= {() => console.log('clicou')}>
-          Clica em mim
-        </Button>
+        <form>
+          <input type="text" value={this.state.value} onChange={(e) => {
+            console.log(e)
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
       </div>
     )
   }
